380a1ac95282146f6254433ef6bc2da72842da7d
user1gh1 <user1gm1@gmail.com>
2024-05-12 19:39:12 +0400

    initial commit

---
+++ myflask.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+
+from flask import Flask, render_template, request
+
+app = Flask(__name__, static_url_path='/static')
+@app.route('/')
+def main():
+    return render_template('main.html')
+
+@app.route('/archive')
+def archive():
+    try:
+        with open('static/base.txt', 'r') as file:
+            content = file.read()
+        return render_template('archive.html', content=content)
+    except FileNotFoundError:
+        return "File not found", 404
+
+    
+
+@app.route('/register', methods=['GET', 'POST'])
+def answer():
+    mypoints = 0
+    if request.method == 'POST':
+        # Get form data
+        fullname = request.form['fullname']
+        dateofbirth = request.form['dateofbirth']
+        education = request.form['education']
+        workinghours = request.form['workinghours']
+        subjectfullname = request.form['subjectfullname']
+        curriculum = request.form['curriculum']
+        manuals = request.form['manuals']
+        
+        answers = {}
+        for key in request.form:
+            if key.startswith('question'):
+                question_number = key.split('_')[-1]
+                answer_option = request.form[key]
+                answers[question_number] = answer_option
+                # Calculate points
+                if answer_option == 'option1':
+                    mypoints += 2
+                elif answer_option == 'option2':
+                    mypoints += 1
+
+        
+        with open('static/base.txt', 'a', encoding='utf-8') as file:
+            file.write(f'fullname: {fullname}, '
+            f'Dateofbirth: {dateofbirth}, '
+            f'Education: {education}, '
+            f'Workinghours: {workinghours}, '
+            f'Subjectfullname: {subjectfullname}, '
+            f'Curriculum: {curriculum}, '
+            f'Manuals: {manuals}, '
+            f'Points: {mypoints}\n')
+        return "Շնորհակալություն հարցմանը մասնակցելու համար!"
+    return render_template('answer_form.html')
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
